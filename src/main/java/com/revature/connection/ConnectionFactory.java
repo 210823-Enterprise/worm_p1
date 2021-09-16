c038e6d375327261a521e5b3c4707530eb6208b8
Erick Johnson <erickjohnson1980@gmail.com>
2021-09-16 18:46:09 -0400

    added ConnectionFactory, added junit and postgressql dependencies in pom.xml

---
+++ src/main/java/com/revature/connection/ConnectionFactory.java
@@ -0,0 +1,76 @@
+package com.revature.connection;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class ConnectionFactory 
+{
+	
+	private BasicDataSource ds;
+	private static final ConnectionFactory connection_factory = new ConnectionFactory();
+	
+	static { 
+	
+					try 
+					{
+						Class.forName("org.postgresql.Driver");
+					} 
+					catch (ClassNotFoundException e) 
+					{
+						e.printStackTrace();
+					}
+		
+	        }
+	
+	private ConnectionFactory() 
+	{
+		
+		try 
+		{
+			Properties props = new Properties();
+			props.load(new FileReader("src/main/resources/application.properties"));
+			ds = new BasicDataSource();
+			ds.setUrl(props.getProperty("url"));
+			ds.setUsername(props.getProperty("username"));
+			ds.setPassword(props.getProperty("password"));
+			ds.setMinIdle(5);
+			ds.setDefaultAutoCommit(false);
+			ds.setMaxOpenPreparedStatements(100);
+			
+		} 
+		catch (IOException e)
+		{
+			//log goes here, custom logger?
+		}
+	}
+	
+	
+	public static ConnectionFactory getInstance() 
+	{
+		return connection_factory;
+	}
+	
+	
+	public Connection getConnection() 
+	{
+		
+		try 
+		{
+			return ds.getConnection();
+		} 
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	
+	
+
+}
